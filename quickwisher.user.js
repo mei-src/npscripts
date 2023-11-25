8fc61e8451ae246404e1a29c29e004ebf6d3ede4
mei-src <git@sl.moth.work>
2023-11-25 05:19:48 -0800

    completely works after reordering

--- quickwisher.user.js
+++ quickwisher.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Starry's Quick Wisher
 // @namespace    https://github.com/mei-src
-// @version      1.5
+// @version      1.6
 // @description  Allows you to save your wish and autoinput on page load.
 // @author       mei-src
 // @match        https://www.neopets.com/wishing.phtml
@@ -12,6 +12,24 @@
 
 (function() {
     'use strict';
+
+    // Inject HTML
+    const settingsMenu = `
+        <style>
+            #qw_quickWisher {display: block;}
+            #qw_wrapper:hover, #qw_wrapper:hover input, #qw_wrapper:hover label {cursor:pointer};
+        </style>
+
+        <div id="qw_quickWisher">
+            <div class="qw_wrapper">
+                <input type="checkbox" id="qw_checkbox">    
+                <label for="qw_checkbox">Remember wish</label>
+            </div>
+        </div>
+    `;
+    const submitElement = document.querySelector('input[value="Make a Wish"]');
+    submitElement.insertAdjacentHTML("afterend", settingsMenu);
+
     // Minimum NP value for donating
     const NP = 21; 
 
@@ -23,38 +41,24 @@
     const checkboxElement = document.querySelector("#qw_checkbox");
     const inputNPElement = document.querySelector("[name='donation']");
     const inputWishElement = document.querySelector("[name='wish']");
-    const submitElement = document.querySelector('input[value="Make a Wish"]');
 
-    // Injected HTML
-    const settingsMenu = `
-        <style>
-            #qw_quickWisher:hover {cursor:pointer};
-        </style>
-
-        <div id="qw_quickWisher">
-            <input type="checkbox" id="qw_checkbox">    
-            <label for="qw_checkbox">📌</label>
-        </div>
-    `;
-
-    // On Page Load, set values if checked
-    submitElement.insertAdjacentHTML("afterend", settingsMenu);
+    // On page load
+    inputNPElement.value = NP;
     if (localStorage.getItem(CHECKVALUE) === "true") {
         checkboxElement.checked = true;
-        inputNPElement.val(NP);
-        inputWishElement.val(localStorage.getItem(WISHVALUE)); 
+        inputWishElement.value = localStorage.getItem(WISHVALUE); 
     } else {
         checkboxElement.checked = false;
     };
 
-    // Save state of checkbox
+    // * eventListener: Save state of checkbox
     checkboxElement.addEventListener("change", function() {
         const isChecked = checkboxElement.checked;
         localStorage.setItem(CHECKVALUE, isChecked ? "true" : "false");
         if (isChecked) localStorage.setItem(WISHVALUE, inputWishElement.value);
     });
 
-    // Save value of wish input
+    // * eventListener: Save value of wish input
     inputWishElement.addEventListener("input", function() {
         if (localStorage.getItem(CHECKVALUE) === "true") {
             localStorage.setItem(WISHVALUE, inputWishElement.value);
